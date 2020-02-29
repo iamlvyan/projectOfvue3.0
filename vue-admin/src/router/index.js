999e5ecb7053d4d9cc9c44a1e163e373b47a5f46
lvyan <1032740533@qq.com>
2020-02-29 22:26:06 +0800

    框架修改

--- vue-admin/src/router/index.js
+++ vue-admin/src/router/index.js
@@ -1,28 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
+const routes = [{
+        path: "/",
+        redirect: 'login'
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () =>
+            import ("../views/login/index.vue")
+    }
+]; /* eslint-disable */
 const router = new VueRouter({
-  routes
+    routes
 });
-
-export default router;
+export default router; /* eslint-disable */
